ad5d110285b19ae76a4b44ced5935613e0db780e
Pranta <baruapranta765@gmail.com>
2026-07-06 22:48:05 +0600

    remove unused imports from gears route file

--- src/module/gears/gears.route.ts
+++ src/module/gears/gears.route.ts
@@ -1,14 +1,11 @@
 import { Router } from "express";
 import { GearsController } from "./gears.controller";
-import { auth } from "../../middlewares/auth";
-import { Role } from "../../../generated/prisma/enums";
 
 const router = Router();
 
 // Public routes
 router.get("/", GearsController.getAllGearsDetails);
 router.get("/:id", GearsController.getSingleGearById);
-
 
 
 export const GearsRouter = router;
