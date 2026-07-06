1669011bdcd1aa2d348165c4e3f33372e6eeb052
Pranta <baruapranta765@gmail.com>
2026-07-06 15:17:13 +0600

    Rental Item modules structure.

---
+++ src/module/rentalItems/rentalItems.controller.ts
@@ -0,0 +1,25 @@
+import { catchAsync } from "../../utils/catchAsync"
+
+const createRentalItems = catchAsync( async() => {
+
+})
+const getAllRentalItems = catchAsync( async() => {
+
+})
+const getSingleRentalItemsByID = catchAsync( async() => {
+// 
+})
+const updateRentalItem = catchAsync( async() => {
+
+})
+const deleteRentalItem = catchAsync( async() => {
+
+})
+
+export const RentalItemsController = {
+    createRentalItems,
+    getAllRentalItems,
+    getSingleRentalItemsByID,
+    updateRentalItem,
+    deleteRentalItem
+}
---
+++ src/module/rentalItems/rentalItems.route.ts
@@ -0,0 +1,16 @@
+import { Router } from "express";
+import { auth } from "../../middlewares/auth";
+import { Role } from "../../../generated/prisma/enums";
+import { RentalItemsController } from "./rentalItems.controller";
+
+const router = Router();
+
+router.get("/", auth(Role.CUSTOMER, Role.PROVIDER, Role.ADMIN), RentalItemsController.getAllRentalItems);
+router.get("/:id", auth(Role.CUSTOMER, Role.PROVIDER, Role.ADMIN), RentalItemsController.getSingleRentalItemsByID);
+
+router.post("/:id", auth(Role.CUSTOMER, Role.PROVIDER, Role.ADMIN), RentalItemsController.createRentalItems);
+router.patch("/:id", auth(Role.PROVIDER, Role.ADMIN), RentalItemsController.updateRentalItem);
+router.delete("/:id", auth(Role.PROVIDER, Role.ADMIN), RentalItemsController.deleteRentalItem);
+
+
+export const RentalItemsRoute = router;
